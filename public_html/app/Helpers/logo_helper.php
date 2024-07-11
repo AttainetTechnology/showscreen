60bfd94f114c0622d696101cc24f7d30c67d7308
MariaAttainet <msancho@attainet.es>
2024-07-11 15:40:19 +0200

    modifica url logo

--- public_html/app/Helpers/logo_helper.php
+++ public_html/app/Helpers/logo_helper.php
@@ -4,7 +4,7 @@
     $urllogo= $url_model->find(1);
             if ($urllogo!=""){
                     $logo_url=$urllogo['url_logo'];
-                    $logo=base_url('/assets/uploads/files')."/".$logo_url;
+                    $logo=base_url('public/assets/uploads/files')."/".$logo_url;
                     return $logo;
             } else {
                  $logo="no_logo.png";
