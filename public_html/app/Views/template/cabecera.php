90a72b9bf6351013997baac74076a54cca485b6a
Attainet <msancho@attainet.es>
2024-07-18 13:42:05 +0200

    Maria: modifico footer menu lateral que me molesta

--- public_html/app/Views/template/cabecera.php
+++ public_html/app/Views/template/cabecera.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Control de presencia</title>
+	<title>Fichaje</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	 <!-- Cargo FAVICON  -->
 <?php
