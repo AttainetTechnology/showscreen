cd6faa3cd7887722daf643abe20acc3f93b864c0
Attainet <msancho@attainet.es>
2024-11-27 13:37:06 +0100

    Maria: muestro producto asociado en producto_necesidad

--- public_html/app/Views/selectProducto.php
+++ public_html/app/Views/selectProducto.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/proveedor.css') ?>?v=<?= time() ?>">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
 </div>
 <div id="productosModalBody">
     <label for="filtrarFamilia" style="margin-bottom:10px;">Filtrar por Familia:</label>
@@ -28,11 +29,13 @@
                         <td><?= esc($producto['nombre_producto']) ?></td>
                         <td>
                             <?php if ($producto['id_producto'] == $id_producto_venta): ?>
-                                <button type="button" class="boton btn btn-danger btn-select" data-id="<?= esc($producto['id_producto']) ?>" data-action="deselect">
+                                <button type="button" class="boton btn btn-danger btn-select"
+                                    data-id="<?= esc($producto['id_producto']) ?>" data-action="deselect">
                                     Desactivar <i class="bi bi-check-square ms-1"></i>
                                 </button>
                             <?php else: ?>
-                                <button type="button" class="boton btn btn-success btn-select" data-id="<?= esc($producto['id_producto']) ?>" data-action="select">
+                                <button type="button" class="boton btn btn-success btn-select"
+                                    data-id="<?= esc($producto['id_producto']) ?>" data-action="select">
                                     Activar <i class="bi bi-square ms-1"></i>
                                 </button>
                             <?php endif; ?>
@@ -43,7 +46,6 @@
         </table>
     <?php endif; ?>
 </div>
-
 
 <script>
     $(document).ready(function() {
@@ -68,39 +70,46 @@
                 $(this).toggle(matchesFamilia && matchesSearch);
             });
         }
+
+        // Evento para manejar la selección/deselección de producto
         $('.btn-select').on('click', function() {
-    var productoId = $(this).data('id');
-    var action = $(this).data('action');
-    var nombreProducto = $(this).closest('tr').find('td:first').text();
-    var idProductoNecesidad = <?= esc($id_producto) ?>;
-    var idProductoVenta = action === 'select' ? productoId : null;
+            var productoId = $(this).data('id');
+            var action = $(this).data('action');
+            var nombreProducto = $(this).closest('tr').find('td:first').text();
+            var idProductoNecesidad = <?= esc($id_producto) ?>;
+            var idProductoVenta = action === 'select' ? productoId : null;
 
-    $.post('<?= base_url('productos_necesidad/actualizarProductoVenta') ?>', {
-        id_producto_necesidad: idProductoNecesidad,
-        id_producto_venta: idProductoVenta
-    }, function(response) {
-        if (response.success) {
-            if (action === 'select') {
-                alert("Has seleccionado " + nombreProducto);
-                $('.btn-select').removeClass('btn-danger').addClass('btn-success')
-                    .html('Activar <i class="bi bi-square ms-1"></i>')
-                    .data('action', 'select');
-                $('button[data-id="' + productoId + '"]').removeClass('btn-success')
-                    .addClass('btn-danger')
-                    .html('Desactivar <i class="bi bi-check-square ms-1"></i>')
-                    .data('action', 'deselect');
-            } else {
-                alert("Has deseleccionado " + nombreProducto);
-                $('button[data-id="' + productoId + '"]').removeClass('btn-danger')
-                    .addClass('btn-success')
-                    .html('Activar <i class="bi bi-square ms-1"></i>')
-                    .data('action', 'select');
-            }
-        } else {
-            alert('Error al realizar la acción.');
-        }
-    }, 'json');
-});
+            $.post('<?= base_url('productos_necesidad/actualizarProductoVenta') ?>', {
+                id_producto_necesidad: idProductoNecesidad,
+                id_producto_venta: idProductoVenta
+            }, function(response) {
+                if (response.success) {
+                    if (action === 'select') {
+                        alert("Has seleccionado " + nombreProducto);
+                        $('.btn-select').removeClass('btn-danger').addClass('btn-success')
+                            .html('Activar <i class="bi bi-square ms-1"></i>')
+                            .data('action', 'select');
+                        $('button[data-id="' + productoId + '"]').removeClass('btn-success')
+                            .addClass('btn-danger')
+                            .html('Desactivar <i class="bi bi-check-square ms-1"></i>')
+                            .data('action', 'deselect');
 
+                        // Actualiza el nombre del producto asociado
+                        $('.asociado-producto').text('Producto asociado: ' + nombreProducto);
+                    } else {
+                        alert("Has deseleccionado " + nombreProducto);
+                        $('button[data-id="' + productoId + '"]').removeClass('btn-danger')
+                            .addClass('btn-success')
+                            .html('Activar <i class="bi bi-square ms-1"></i>')
+                            .data('action', 'select');
+
+                        // Limpia el nombre del producto asociado
+                        $('.asociado-producto').text('No hay producto de venta seleccionado');
+                    }
+                } else {
+                    alert('Error al realizar la acción.');
+                }
+            }, 'json');
+        });
     });
 </script>
